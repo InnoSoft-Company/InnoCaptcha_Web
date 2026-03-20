9de89cfb871b10a19c2a72929acbc1112322f75f
Mohammed Ahmed Ghanam <midoghanam@hotmail.com>
2026-03-20 15:36:13 +0200

    Starting Auth System

---
+++ authentication/admin.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+from .models import *
+
+User = get_user_model()
+
+class UserAdmin(BaseUserAdmin):
+  def save_model(self, request, obj, form, change):
+    obj.username, obj.email = obj.username.lower(), obj.email.lower()
+    super().save_model(request, obj, form, change)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(OAuth)
---
+++ authentication/views.py
@@ -0,0 +1,5 @@
+from rest_framework.views import APIView
+
+class LoginView(APIView): 
+  def post(self, request):
+    pass
