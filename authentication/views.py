2df1f98020aec70a9edd1dc6ffa9b1719a2c93c2
Mohammed Ahmed Ghanam <midoghanam@hotmail.com>
2026-03-20 15:48:12 +0200

    Collecting Statics & Creating ( User Serializer, Login & Registration) views

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+User = get_user_model()
 
 class LoginView(APIView): 
   def post(self, request):
-    pass
+    data = UserSerializer(request.user)
+    data.is_valid()
+    data = data.data
+
+class RegisterView(APIView):
+  def post(self, request):
+    data = UserSerializer(request.user)
+    data.is_valid()
+    data = data.data
+    user = User.objects.create_user(**data)
